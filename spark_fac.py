ddcf21a2e7cd9caa6001391134c876814e4db84b
cm5168 <cm19920214@gmail.com>
2016-02-22 19:35:53 -0500

    Matrix Factorization

--- spark_fac.py
+++ spark_fac.py
@@ -2,7 +2,8 @@ from __future__ import print_function
 from pyspark.sql import SQLContext
 from pyspark import SparkContext
 from pyspark.ml.recommendation import ALS
-
+import time
+start_time = time.time()
 sparkC = SparkContext()
 sqlC = SQLContext(sparkC)
 trainData = sparkC.textFile("Data/trainData.txt").map(lambda line: line.split("\t"))
@@ -11,15 +12,15 @@ testData = sparkC.textFile("Data/testData.txt").map(lambda line: line.split("\t"
 trainDataFrame = sqlC.createDataFrame(trainData,["user","item","rating"])
 testDataFrame = sqlC.createDataFrame(testData,["user","item"])
 
-als = ALS(rank = 10,maxIter = 5)
+als = ALS(rank = 10,maxIter = 30)
 
-model = als.fit(trainDataFram)
+model = als.fit(trainDataFrame)
 
 predTestData = model.transform(testDataFrame)
 
 prediction = sorted(predTestData.collect(), key = lambda r: int(r[0]))
 
-with open("Result/prediction1.txt","w") as predFile:
+with open("Results/prediction1.txt","w") as predFile:
     for line in prediction:
         if line[2]!=line[2]:
             temp_str = "None"
@@ -28,3 +29,4 @@ with open("Result/prediction1.txt","w") as predFile:
         predFile.write(str(line[0])+"|"+str(line[1])+"|"+temp_str+"\n")
 
 sparkC.stop()
+print("Spend %d s"%(time.time()-start_time))
